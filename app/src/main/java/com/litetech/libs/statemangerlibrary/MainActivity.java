74fb486363743e2762eac5f33f4ecad7851ea3ff
Johnson Osei Yeboah <johnyoat@gmail.com>
2018-06-12 10:48:09 +0000

    First Commit

---
+++ app/src/main/java/com/litetech/libs/statemangerlibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.litetech.libs.statemangerlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

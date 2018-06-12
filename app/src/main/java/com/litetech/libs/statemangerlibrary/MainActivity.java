8225532bc207fb4430136c6453bfd0958f75e0d1
Johnson Osei Yeboah <johnyoat@gmail.com>
2018-06-12 12:34:06 +0000

    Lcom/google/gson/Gson Bug Fixed

--- app/src/main/java/com/litetech/libs/statemangerlibrary/MainActivity.java
+++ app/src/main/java/com/litetech/libs/statemangerlibrary/MainActivity.java
@@ -3,6 +3,14 @@ package com.litetech.libs.statemangerlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.litetech.libs.statemanager.StateManger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +18,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StateManger.init(this);
+
+        AppCompatButton save = findViewById(R.id.save);
+        AppCompatButton retrieve = findViewById(R.id.retrieve);
+        AppCompatButton delete = findViewById(R.id.del);
+        final AppCompatTextView storedText = findViewById(R.id.storedTExt);
+        final AppCompatEditText text = findViewById(R.id.text);
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(text.getText().toString())){
+                    StateManger.getInstance().saveObject("text",text.getText().toString());
+                    Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        retrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stText = StateManger.getInstance().getObject("text",String.class);
+                if (stText != null){
+                    storedText.setText(stText);
+                }else {
+                    Toast.makeText(getApplicationContext(),"No text found",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StateManger.getInstance().deleteObject("text");
+                Toast.makeText(getApplicationContext(),"Data deleted",Toast.LENGTH_SHORT).show();
+                storedText.setText("");
+            }
+        });
+
+
     }
 }
 
